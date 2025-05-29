94dff3f1dbfcbc5d540265bb66cebfe5d3c5412b
ArhyaSaha <arhyapratimsaha28@gmail.com>
2025-05-29 23:57:05 +0530

    chore(desktop): add custom windows XP cursor.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { WindowProvider } from "./context/WindowContext";
 const App = () => {
   return (
     <WindowProvider>
-      <div className="bg-[url('/Background.jpg')] bg-cover bg-center flex flex-col h-screen z-0">
+      <div className="bg-[url('/Background.jpg')] bg-cover bg-center flex flex-col h-screen z-0" style={{ cursor: 'url("/cursor/default_arrow.cur"), auto' }}>
         <Desktop />
 
         <StatusBar />
